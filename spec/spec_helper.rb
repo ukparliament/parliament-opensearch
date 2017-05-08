e2dad22756a72fbb0ad82eb0c7e0bea90f30ca87
Rebecca Appleyard <rklappleyard@gmail.com>
2017-05-08 10:16:19 +0100

    [WEBSITE-959] Allow multiple urls in the template document
    * Changed implementation to store different template urls and types from the description, replacing the single @base_url.
    * Updated documentation.
    * Fixed some of the rubocop violations.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,16 +10,25 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'parliament'
 require 'parliament/open_search'
 
+require 'webmock'
 require 'webmock/rspec'
 require 'vcr'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+# Setup the initial description file request
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock # or :fakeweb
   config.configure_rspec_metadata!
+
+  # Dynamically filter our sensitive information
+  config.filter_sensitive_data('<AUTH_TOKEN>') { ENV['OPENSEARCH_AUTH_TOKEN'] }
 end
 
 RSpec.configure do |config|
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
